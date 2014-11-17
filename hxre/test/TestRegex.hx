a8b8a0ab6f43e1c1bd981ed8795b91453655db55
Ryusei Yamaguchi <mandel59@gmail.com>
2014-11-18 05:06:39 +0900

    add tests

--- hxre/test/TestRegex.hx
+++ hxre/test/TestRegex.hx
@@ -1,16 +1,22 @@
 package hxre.test;
 
 class TestRegex extends haxe.unit.TestCase {
-	public function testRegex() {
+	public function testRegex1() {
 		assertTrue(new Regex("a...b").test("abababbb"));
 		assertTrue(new Regex("b?<(?:ab)*>a?").test("b<abab>a"));
-		{
-			var re = new Regex("^(?:ab|c){3,5}$");
-			assertFalse(re.test("abc"));
-			assertTrue(re.test("abcab"));
-			assertTrue(re.test("abccabab"));
-			assertFalse(re.test("abcabcabc"));
-		}
 		assertTrue(new Regex("^\\\\$").test("\\"));
+	}
+
+	public function testRegex2() {
+		var re = new Regex("^(?:ab|c){3,5}$");
+		assertFalse(re.test("abc"));
+		assertTrue(re.test("abcab"));
+		assertTrue(re.test("abccabab"));
+		assertFalse(re.test("abcabcabc"));
+	}
+
+	public function testDigit() {
+		assertTrue(new Regex("^\\d{10}$").test("0123456789"));
+		assertFalse(new Regex("\\D").test("0123456789"));
 	}
 }
